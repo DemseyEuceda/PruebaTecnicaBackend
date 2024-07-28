4d4a2f734b3b65a17a40263179c972c8b8192228
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2024-07-28 14:04:02 -0600

    se valida los datos de la peticion con zoc
    se realiza una función en schemas.ts el cual hace una validación para que los datos lleguen de la forma correcta

--- api/v1/example/helpers/schemas.ts
+++ api/v1/example/helpers/schemas.ts
@@ -4,3 +4,10 @@ export const validateLoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const validateDataCotizacion = z.object({
+  edad : z.number().int().refine(edad => edad >= 18 && edad <= 66),
+  sumaAsegurada : z.number(),
+  sexo : z.string().regex(/^[MFmf]$/),
+  fumador : z.boolean()
+})
