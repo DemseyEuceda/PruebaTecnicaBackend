e342dd2e591e503c8482f3187548c8e5f3e807af
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2024-07-28 21:23:04 -0600

    funciones necesarias en el controlador
    agregue las funciones en el archivo functions.ts para acceder a ellas desde el controlador, estas funciones son las que me hacen la peticion a la api de seguros plus y también hacen el calculo segun el Json de Vida Insure

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -1,3 +1,13 @@
+
+import fetch from "node-fetch";
+import { join } from "path";
+import dotenv from 'dotenv';
+import { readFileSync } from "fs";
+import persona from "../api/v1/example/interfaces/persona";
+
+dotenv.config();
+
+
 export async function asyncForEach(array: any, callback: any) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
@@ -52,4 +62,54 @@ export async function passwordGenerator(largo: number): Promise<string> {
     .join('');
 
   return contrasena;
+}
+
+
+export async function CotizacionSegurosPlus(edad : number, sumaAsegurada : number, sexo : string) {
+  //const filepath = join(__dirname, '../tasas/tasasVidaInsure.json');
+  const apiUrl = process.env.API_URL as string;
+  const user = process.env.API_USER as string;
+  const password = process.env.API_PASSWORD as string;
+  let data = {
+    edad :edad, 
+    sumaAsegurada : sumaAsegurada, 
+    sexo : sexo
+  }
+  let response = await fetch(apiUrl, {
+    method : 'POST',
+    headers : {
+      'Authorization': `Basic ${Buffer.from(`${user}:${password}`).toString('base64')}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  
+
+  return response.json();
+
+  
+}
+
+export function cotizacionVidainsure(edad : number, sumaAsegurada : number, sexo : string, fumador : boolean){
+  const filepath = join(__dirname, '../tasas/tasasVidaInsure.json');
+  const personas = loadData(filepath);
+  const personaBuscada = personas.find(item => item.Edad = edad);
+  let primaAnual : number = 0;
+  console.log(personaBuscada)
+  if(typeof personaBuscada !='undefined'){
+    if(fumador && sexo == "M")
+      primaAnual = (personaBuscada.hombreFumador/1000)*sumaAsegurada;
+    else if(!fumador && sexo == "M")
+      primaAnual = (personaBuscada.hombreNoFumador/1000)*sumaAsegurada;
+    else if(fumador  && sexo == "F")
+      primaAnual = (personaBuscada.mujerFumadora/1000)*sumaAsegurada;
+    else if(!fumador  && sexo == "F")
+      primaAnual = (personaBuscada.mujerNoFumadora/1000)*sumaAsegurada;
+    }
+  return primaAnual;
+}
+
+
+export function loadData(filepath : string) : persona[]{
+  return JSON.parse(readFileSync(filepath, 'utf-8'));
 }
